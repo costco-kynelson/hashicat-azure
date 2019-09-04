f9155aa418c13696fdd00ef9ad3cb6b59893da71
Kyler Nelson <kynelson@costco.com>
2019-09-04 20:38:49 +0000

    initial push

--- remote_backend.tf
+++ remote_backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "remote" {
     hostname = "app.terraform.io"
-    organization = "hashicorp-workshop"
+    organization = "costco-kynelson-sandbox"
     workspaces {
-      name = "YOURWORKSPACE"
+      name = "hashicat-azure"
     }
   }
 }
